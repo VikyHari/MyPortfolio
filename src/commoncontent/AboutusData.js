895dc2ae7cac106c55f6b430df83992e6d6d75d7
vigneshwar <vigneshwar@cdp360.com>
2024-06-17 14:43:19 +0530

    My portfolio

---
+++ src/commoncontent/AboutusData.js
@@ -0,0 +1,151 @@
+export const TabContent = [
+    {
+        id: 1,
+        name: "Personal Info"
+    },
+    {
+        id: 2,
+        name: "Qualifications"
+    },
+    {
+        id: 3,
+        name: "Skills"
+    }
+]
+
+
+export const PersonalContent = {
+    name: "kalaisurya",
+    phoneno: "+91 8778377119",
+    emailid: "kalaimca685@gmail.com",
+    dob: "Born on 12 June,1998",
+    deggre: "Master on Computer Science",
+    address: "chennai",
+    desc: "I specialize in crafting intuitive websites with cutting-edge techonogy, delivering dynamic and enaging user experiences.",
+    languageName: "Language Skill",
+    MyAwesomeJourney: 'My Awesome Journey',
+    tools: "Tools I Use Everyday",
+    languages: [
+        {
+            id: 1,
+            name: "English"
+        },
+        {
+            id: 2,
+            name: "Tamil"
+        },
+        {
+            id: 3,
+            name: "Telugu"
+        }
+    ]
+}
+
+
+export const Qualifications = [
+    {
+        id: 1,
+        name: "Master Of Computer Application",
+        dgname: "( MCA )",
+        clgName: "Karpagam Academy Of Higher Education",
+        year: "2018 - 2020",
+        place: "Coimbatore",
+        calicon: <i class="fa-regular fa-calendar cal-icon"></i>
+    },
+    {
+        id: 2,
+        name: "Bachlore of Computer Application",
+        dgname: "( BCA )",
+        year: "2015 - 2018",
+        clgName: "Sri Vidya Mandir Arts and Science College",
+        place: "Krishnagiri",
+        calicon: <i class="fa-regular fa-calendar cal-icon"></i>
+
+    },
+    {
+        id: 3,
+        name: "Higher Secondary School",
+        dgname: "( HSC )",
+        year: "2013 - 2015",
+        clgName: "Government Boys Higher Secondary School",
+        place: "Krishnagiri",
+        calicon: <i class="fa-regular fa-calendar cal-icon"></i>
+
+    },
+    {
+        id: 4,
+        name: "Secondary School Leaving Certificate",
+        dgname: "( SSLC )",
+        year: "2013",
+        clgName: "Government Higher Secondary School",
+        place: "Krishnagiri",
+        calicon: <i class="fa-regular fa-calendar cal-icon"></i>
+
+    }
+]
+
+
+export const Skills = [
+    {
+        id: 1,
+        name: "Reactjs"
+    },
+    {
+        id: 2,
+        name: "Nextjs"
+    },
+    {
+        id: 3,
+        name: "React Native"
+    },
+    {
+        id: 4,
+        name: "Nodejs"
+    },
+    {
+        id: 5,
+        name: "Context Api"
+    },
+    {
+        id: 6,
+        name: "Redux"
+    },
+    {
+        id: 5,
+        name: "Javascript"
+    },
+    {
+        id: 6,
+        name: "HTML"
+    },
+    {
+        id: 7,
+        name: "CSS"
+    },
+    {
+        id: 8,
+        name: "SASS"
+    },
+    {
+        id: 9,
+        name: "UI/UX"
+    },
+    {
+        id: 10,
+        name: "Bootstrap"
+    },
+    {
+        id: 11,
+        name: "Github"
+    },
+    {
+        id: 12,
+        name: "Gitlab"
+    },
+    {
+        id: 13,
+        name: "Responsive UI Designs"
+    },
+
+
+]
---
+++ src/commoncontent/ContactData.js
@@ -0,0 +1,24 @@
+export const ContactDatas = {
+    conntactList: [
+        {
+            id: 1,
+            name: "Chennai",
+            image: <i class="fa-solid fa-location-dot icon-color"></i>
+        },
+        {
+            id: 2,
+            name: "+91 8778377119",
+            image: <i class="fa-solid fa-phone icon-color"></i>
+        },
+        {
+            id: 3,
+            name: "kalaimca685@gmail.com",
+            image: <i class="fa-solid fa-envelope icon-color"></i>
+        }
+    ],
+    name: "Name",
+    email: "Email",
+    message: "Messag",
+    help: "Let’s talk how I can help you!",
+    getintouch:"Get in touch"
+}
